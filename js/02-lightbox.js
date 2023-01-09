fef53f5da206a6470ad07ae21207e84295a0824b
Natali-Stankevych <rusakovanatali94@gmail.com>
2023-01-09 19:09:34 +0200

    add 02 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const imegesContainer = document.querySelector(".gallery");
+
+const marcup = galleryItems.map(({ preview, original, description }) =>
+  ` <a class="gallery__item" 
+  href="${original}">
+  <img class="${original}" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`)
+  .join(" ");
+
+imegesContainer.insertAdjacentHTML("beforeend", marcup);
+
+imegesContainer.addEventListener('click', (eve) => {
+    eve.preventDefault();
+    if (eve.target.nodeName !== "IMG") {
+        return
+    }
+    new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    })
+
+})
